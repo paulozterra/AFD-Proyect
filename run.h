629c742e52378f36f556f0fbf086d4ce4fb9e6b0
paulozterra <66433825+paulozterra@users.noreply.github.com>
2021-06-27 15:10:45 -0500

    Update run.h

--- run.h
+++ run.h
@@ -7,12 +7,17 @@
 #include "AFD.h"
 
 
+
 void run(){
     int n;
     int ind_inicial;
     int n_final;
     int* f_states;
-    cin >> n >> ind_inicial >> n_final;
+
+    do{
+        cin >> n >> ind_inicial >> n_final;
+    }while(n < ind_inicial || n < n_final)
+    
     f_states = new int[n_final];
     for (int i=0;i<n_final;i++){
         cin >> f_states[i];
@@ -22,10 +27,14 @@ void run(){
     int caracter;
     int qfin;
     for (int i=0; i<2*n; i++){
-        cin >> qini >> caracter >> qfin;
+        do{
+            cin >> qini >> caracter >> qfin;
+            }while(caracter!=1 and caracter!=0)
+        
         afd.crear_conecction(qini, caracter, qfin);
     }
     afd.MIN();
 }
 
 #endif //AFD_PROYECT_RUN_H
+
